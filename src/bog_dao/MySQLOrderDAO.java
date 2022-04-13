8027775f790af5eea087a0bb4da07ba76e81eb66
irideirene <100774103+irideirene@users.noreply.github.com>
2022-04-13 13:31:57 +0200

    Update MySQLOrderDAO.java

--- src/bog_dao/MySQLOrderDAO.java
+++ src/bog_dao/MySQLOrderDAO.java
@@ -16,40 +16,191 @@
 
 package bog_dao;
 
+import bog_models.Customer;
 import java.util.ArrayList;
-
 import bog_models.Order;
+import bog_models.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
+
 
 public class MySQLOrderDAO implements OrderDAO {
+    
+    final String INSERT = "INSERT INTO orders (orderID, productID, customerEmail, productQuantity, subtotal, creationDateTime, handlingTime, isSent) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    final String UPDATE = "UPDATE orders SET orderID = ?, productID = ?, customerEmail = ?, productQuantity = ?, subtotal = ?, creationDateTime = ?, handlingTime = ?, isSent = ?";
+    final String DELETE = "DELETE FROM orders WHERE orderID = ?";
+    final String GETALL = "SELECT orderID, productID, customerEmail, productQuantity, subtotal, creationDateTime, handlingTime, isSent FROM orders";
+    final String GETONE = "SELECT orderID, productID, customerEmail, productQuantity, subtotal, creationDateTime, handlingTime, isSent FROM orders WHERE orderID = ?";
 
+    private Connection conn;
+    
+    public MySQLOrderDAO(Connection conn){
+        this.conn=conn;}
+    
     @Override
     public void create(Order insertado) throws DAOException {
-        // TODO Auto-generated method stub
-        
+    PreparedStatement stat = null;
+        try {
+            stat = conn.prepareStatement(INSERT);
+            stat.setInt(1, insertado.getorderID());
+            stat.setString(2, insertado.getProduct().toString());
+            stat.setString(3, insertado.getCustomer().toString());
+            stat.setInt(4, insertado.getproductQuantity());
+            stat.setDouble(5, insertado.getSubtotal());
+            stat.setString(6, insertado.getcreationDataTime().toString());
+            stat.setInt(7, insertado.gethandlingTime());
+            stat.setBoolean(8,insertado.isCancellable());
+            if (stat.executeUpdate() ==0){
+                throw new DAOException("Puede que el pedido no se haya guardado");
+        }
+        } catch (SQLException ex) {
+            throw new DAOException("Error en SQL", ex);
+        } finally {
+            if (stat != null){
+                try {
+                    stat.close();
+                }catch (SQLException ex){
+                    throw new DAOException ("Error en SQL", ex);
+                }
+            }
+        }        
     }
+    
+        private Order convertir(ResultSet rs) throws SQLException{
+        Product product = Product.valueOf(rs.getString("productID"));
+        String productID = rs.getString("productID");
+        Customer customer = Customer.valueOf(rs.getString("customerEmail"));
+        int productQuantity = rs.getInt("productQuantity");
+        Double subtotal = rs.getDouble("subtotal");
+        LocalDateTime creationDataTime =  rs.getTimestamp(6).toLocalDateTime();
+        int handlingTime = rs.getInt("handlingTime");
+        Boolean isSent=rs.getBoolean("isSent");
+        Order order = new Order (product, customer, productQuantity);
+        return order;  
+        }
 
     @Override
     public Order readOne(String id) throws DAOException {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        Order order = null;
+        try{
+            stat = conn.prepareStatement(GETONE);
+            stat.setString(1, id);
+            rs = stat.executeQuery();
+            if (rs.next()){
+                order = convertir(rs);
+            } else{
+                throw new DAOException("No se ha encontrado este pedido");
+        }
+    }catch (SQLException ex){
+        throw new DAOException ("Error en SQL", ex);
+    } finally {
+            if(rs != null){
+                try{
+                    rs.close();
+                } catch(SQLException ex){
+                    new DAOException ("Error en SQL", ex);
+            }
+        }
+            if (stat != null) {
+                try {
+                    stat.close();
+                } catch (SQLException ex) {
+                    new DAOException("Error en SQL", ex);
+                }
+            }
+    }
+    return order;
     }
 
     @Override
     public ArrayList<Order> readAll() throws DAOException {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            stat = conn.prepareStatement(GETALL);
+            rs = stat.executeQuery();
+            while (rs.next()){
+                orders.add(convertir(rs));
+            }
+        } catch (SQLException ex) {
+            throw new DAOException("Error en SQL", ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    new DAOException("Error en SQL", ex);
+                }
+            } 
+            if (stat != null) {
+                try{
+                    stat.close();
+                }catch (SQLException ex){
+                    new DAOException("Error en SQL", ex);
+                }
+            }
+        }
+        return orders;
     }
 
     @Override
     public void update(Order modificado) throws DAOException {
-        // TODO Auto-generated method stub
-        
+        PreparedStatement stat=null;
+        try{
+            stat = conn.prepareStatement(UPDATE);
+            stat.setInt(1, modificado.getorderID());
+            stat.setString(2, modificado.getProduct().getproductID());
+            stat.setString(3, modificado.getCustomer().getEmail());
+            stat.setInt(4, modificado.getproductQuantity());
+            stat.setDouble(5, modificado.getSubtotal());
+            stat.setString(6, modificado.getcreationDataTime().toString());
+            stat.setInt(7,modificado.gethandlingTime());
+            stat.setBoolean(8,modificado.orderSent());
+
+            if (stat.executeUpdate() ==0){
+                throw new DAOException("Puede que el pedido no se haya guardado");
+        }
+        } catch (SQLException ex){
+            throw new DAOException ("Error de SQL", ex);
+        } finally {
+           if (stat !=null){
+           try{
+           stat.close();
+           }catch (SQLException ex) {
+               throw new DAOException ("Error de SQL", ex);
+                }    
+           }
+        }        
     }
 
     @Override
     public void delete(Order eliminado) throws DAOException {
-        // TODO Auto-generated method stub
-        
+        PreparedStatement stat = null;
+        try{
+            stat = conn.prepareStatement(DELETE);
+            stat.setInt(1, eliminado.getorderID());
+            if (stat.executeUpdate() ==0) {
+                throw new DAOException ("Puede que el pedido no se haya eliminado");
+            }
+            } catch (SQLException ex){
+                throw new DAOException ("Error de SQL", ex);
+            } finally {
+                if (stat != null){
+                    try{
+                        stat.close();
+                    }catch (SQLException ex){
+                        throw new DAOException("Error de SQL", ex);
+                    }
+                }
+            }
+    
+            
     }
     
 }
